680eaf6e025bbe7d5bb7f42503b8fd41e71decc6
a-milek <anna@milek.eu.org>
2024-09-06 12:36:55 +0200

    Cancelling Fetch Requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import ProductList from "./components/ProductList";
-import axios from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
 
 interface User {
   id: number;
@@ -12,10 +12,20 @@ function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
   useEffect(() => {
+    const controller = new AbortController();
+
+    //get->promise->res/err
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
       .then((response) => console.log(setUsers(response.data)))
-      .catch((err) => setError(err.message)); //returns a promise
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      }); //returns a promise
+
+    return () => controller.abort();
   }, []);
 
   return (
